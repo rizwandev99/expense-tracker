3effa7bc525fafb0fde4186edb870f34d020ff4e
rizwandev99 <rizwandev99@gmail.com>
2024-03-12 15:19:35 +0530

    form post

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,4 +1,10 @@
-import React, { useContext, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import classes from "./Welcome.module.css";
 import { NavLink } from "react-router-dom";
 import AuthContext from "../../store/AuthContext";
@@ -7,27 +13,94 @@ const Welcome = () => {
   const ctx = useContext(AuthContext);
 
   const [category, setCategory] = useState("");
-  const [money, setMoney] = useState();
-  const [description, setDescription] = useState();
+  const [money, setMoney] = useState("");
+  const [description, setDescription] = useState("");
+  const [data, setData] = useState();
 
   const moneyInputRef = useRef();
   const descriptionInputRef = useRef();
+  const categoryInputRef = useRef();
 
-  let expCategory;
-  const handleChange = (event) => {
-    expCategory = event.target.value;
-  };
+  // let expCategory;
+  // const handleChange = (event) => {
+  //   expCategory = event.target.value;
+  // };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     const moneyRef = moneyInputRef.current.value;
     const descriptionRef = descriptionInputRef.current.value;
+    const categoryRef = categoryInputRef.current.value;
 
-    setMoney(() => moneyRef);
-    setDescription(() => descriptionRef);
-    setCategory(() => expCategory);
+    await setMoney(moneyRef);
+    await setDescription(descriptionRef);
+    await setCategory(categoryRef);
+
+    console.log("Hi", category, money, description);
+
+    setData((prevData) => {
+      var randomKey = Math.random().toString(36).substring(2);
+
+      let item = {
+        ...prevData,
+        [randomKey]: {
+          money: money,
+          description: description,
+          category: category,
+        },
+      };
+      return item;
+    });
+
+    fetch(
+      "https://expense-tracker-d62f0-default-rtdb.firebaseio.com/expense.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          money: money,
+          description: description,
+          category: category,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Email verify failed");
+        }
+      })
+      .then((res) => {
+        console.log("Expense successfully added", res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
+
+  useEffect(() => {
+    fetch(
+      "https://expense-tracker-d62f0-default-rtdb.firebaseio.com/expense/.json"
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Email verify failed");
+        }
+      })
+      .then((res) => {
+        console.log(" Get Expense successfully ", res);
+        setData(() => res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const verifyEmail = () => {
     fetch(
@@ -63,11 +136,11 @@ const Welcome = () => {
       <div className={classes.h1}>
         <h4>Welcome to the Expense Tracker!!!</h4>
       </div>
-
+      <hr />
       <div className={classes.main}>
         Your profile is incomplete <NavLink to="/profile">Complete now</NavLink>
       </div>
-
+      <hr />
       <div className={classes.main}>
         Please verify your E-mail{" "}
         <NavLink to="/email" onClick={verifyEmail}>
@@ -77,17 +150,17 @@ const Welcome = () => {
       <hr />
       <form onSubmit={handleSubmit}>
         <br />
-        <label htmlFor="money">Enter money:</label>
+        <label htmlFor="money">Enter money : </label>
         <input type="number" id="money" ref={moneyInputRef} />
         <br />
         <br />
-        <label htmlFor="description">Description</label>
+        <label htmlFor="description">Description : </label>
         <input type="text" id="description" ref={descriptionInputRef} />
         <br />
         <br />
         <label>
-          Category:
-          <select value={expCategory} onChange={handleChange}>
+          Category :{/* <select value={expCategory} onChange={handleChange}> */}
+          <select ref={categoryInputRef}>
             <option value="">Select...</option>
             <option value="food">Food</option>
             <option value="petrol">Petrol</option>
@@ -103,9 +176,17 @@ const Welcome = () => {
         <br />
         <h3>Below are your enterd Expense:</h3>
         <br />
-        <h4>Money : {money}</h4>
-        <h4>Description : {description}</h4>
-        <h4>Category : {category}</h4>
+        {data &&
+          Object.keys(data).map((key) => {
+            const item = data[key];
+            return (
+              <div key={key}>
+                <h4>Money : {item.money}</h4>
+                <h4>Description : {item.description}</h4>
+                <h4>Category : {item.category}</h4>
+              </div>
+            );
+          })}
       </div>
     </>
   );
